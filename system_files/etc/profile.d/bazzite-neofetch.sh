fd79025b89cf11a5eb9ae4e7d79e4f85d338074a
Alfred Gamulo <alfred.gamulo@gmail.com>
2024-09-02 23:09:07 -0400

    customize neofetch

---
+++ system_files/etc/profile.d/bazzite-neofetch.sh
@@ -0,0 +1,4 @@
+alias neofetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc --logo "$(ls /usr/share/ublue-os/bazzite/*.sixel | shuf -n 1)"'
+alias neowofetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc --logo "$(ls /usr/share/ublue-os/bazzite/*.sixel | shuf -n 1)"'
+alias hyfetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc --logo "$(ls /usr/share/ublue-os/bazzite/*.sixel | shuf -n 1)"'
+alias fastfetch='/usr/bin/fastfetch -c /usr/share/ublue-os/bazzite/fastfetch.jsonc --logo "$(ls /usr/share/ublue-os/bazzite/*.sixel | shuf -n 1)"'
