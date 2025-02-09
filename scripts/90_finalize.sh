c000f8248451ee44ba965b63330f6834f17b0ff4
Alfred Gamulo <alfred.gamulo@gmail.com>
2025-02-08 21:05:08 -0400

    revert: remove image-info override in 90_finalize.sh (#9)
    * Update 90_finalize.sh
    
    because of https://github.com/ublue-os/bazzite/commit/38d69145e1b4831b6e89520db1d97ca55d9e643b#diff-79cc02f955e90bbc7f38e3b69ea3f6745bc01ddb1ac9e1a05abba61b9f7b32bc
    
    customizing image details is too complicated and not necessary.

--- scripts/90_finalize.sh
+++ scripts/90_finalize.sh
@@ -5,7 +5,3 @@ set -ouex pipefail
 # Clean up the yum repos (updates are baked into new images)
 rm -f /etc/yum.repos.d/docker-ce.repo
 rm -f /etc/yum.repos.d/vscode.repo
-
-/usr/libexec/containerbuild/image-info
-/usr/libexec/containerbuild/build-initramfs
-/usr/libexec/containerbuild/cleanup.sh
