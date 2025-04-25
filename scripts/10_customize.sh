1ec36455b76893fe7d8d8298a358c17d1781296b
Alfred Gamulo <alfred.gamulo@gmail.com>
2025-04-25 10:27:11 -0400

    feat: cleanup vestigial customization (#16)

--- scripts/10_customize.sh
+++ scripts/10_customize.sh
@@ -5,3 +5,14 @@ set -ouex pipefail
 echo "import \"/usr/share/ublue-os/just/100-quantumqat.just\"" >> /usr/share/ublue-os/justfile
 
 rm -f /etc/skel/.config/autostart/steam.desktop
+
+cat > /usr/share/ublue-os/custom-image-info.json <<EOF
+{
+  "image-name": "quantumqat",
+  "image-vendor": "alfredgamulo",
+  "image-ref": "ostree-image-signed:docker://ghcr.io/alfredgamulo/quantumqat",
+  "base-image-name": "bazzite",
+  "version": "$VERSION_TAG",
+  "version-pretty": "$VERSION_PRETTY"
+}
+EOF
